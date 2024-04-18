a6c2fe29ab20308d75b3399eae80c61bf86bbaa5
Azatovv <baktiar0807@gmail.com>
2024-04-18 18:46:02 +0600

    i am Bakhtiyar

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const tag = document.querySelector("button");
 const btn = document.querySelector('h4');
+
+tag.onclick=()=>{
+    const random=Math.floor(Math.random()*100)
+    btn.innerHTML=random
+    if (random>30 && random<40){
+        alert("вы угадали")
+    }
+}
